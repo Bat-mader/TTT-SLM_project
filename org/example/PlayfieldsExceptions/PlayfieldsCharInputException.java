4dedd759dd2cb59524f9aeda3cd7de0532dab128
klestisala <ic23b047@technikum-wien.at>
2024-01-09 13:37:31 +0100

    Feature comming

---
+++ org/example/PlayfieldsExceptions/PlayfieldsCharInputException.java
@@ -0,0 +1,10 @@
+package org.example.PlayfieldsExceptions;
+
+import org.example.PlayfieldsException;
+
+public class PlayfieldsCharInputException extends PlayfieldsException {
+
+    public PlayfieldsCharInputException(String message) {
+        super(message);
+    }
+}
